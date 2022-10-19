cc61c24bfde6b83fe9f9c2a7e4f10278b82255ee
Alben <albenbustamante@gmail.com>
2022-10-19 12:17:50 -0500

    Contact entity defined

---
+++ src/main/java/com/alnicode/contacts/entity/Contact.java
@@ -0,0 +1,44 @@
+package com.alnicode.contacts.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "contacts")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "contact_id")
+    private Long id;
+
+    @Column(length = 60, nullable = false)
+    private String name;
+
+    @Column(length = 10, nullable = false, unique = true)
+    private String phone;
+
+    @Column(length = 240)
+    private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return Objects.equals(id, contact.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
