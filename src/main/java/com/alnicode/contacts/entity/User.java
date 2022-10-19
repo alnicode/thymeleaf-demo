f7edf5173f3fd5f3376d786bd90a4978d9457c7b
Alben <albenbustamante@gmail.com>
2022-10-19 11:56:15 -0500

    User entity defined

---
+++ src/main/java/com/alnicode/contacts/entity/User.java
@@ -0,0 +1,50 @@
+package com.alnicode.contacts.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(length = 120, nullable = false)
+    private String name;
+
+    @Column(length = 240, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 10, nullable = false, unique = true)
+    private String phone;
+
+    @Column(length = 30, nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
